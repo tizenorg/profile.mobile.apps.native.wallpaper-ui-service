4a785aa992084c76d7de39eedb52cc6b98f6e541
Andrey Klimenko <and.klimenko@samsung.com>
2015-11-04 10:54:39 +0200

    TizenRefApp-4934 initial commit of wallpaper-service
    Change-Id: I56f67987c21a8078f1043479627c82173450ca64
    Signed-off-by: Andrey Klimenko <and.klimenko@samsung.com>

---
+++ include/wallpaper-ui-service.h
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 20014-2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @{
+ */
+
+#ifndef __WALLPAPER_UI_SERVICE_H__
+#define __WALLPAPER_UI_SERVICE_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <Eina.h>
+#include <Evas.h>
+#include "wallpaper-ui-service-debug.h"
+#include <E_DBus.h>
+
+#define PKGNAME "wallpaper-ui-service"
+
+//#define _X(x) (x*elm_config_scale_get())
+#define _EDJ(o)					elm_layout_edje_get(o)
+#define SYS_STRING(str)			dgettext("sys_string", str)
+#define APP_STRING(str)			dgettext(PKGNAME, str)
+#define NOT_LOCALIZED(str)		(str)
+
+#define _(str)			gettext(str)
+
+#define _NOT_LOCALIZED(str) (str)
+
+#define GENGRID_ITEM_SIZE (126)
+#define MAX_LENGTH_LINE 2001
+#define MAX_LENGTH_STRING 4096
+#define MAX_MULTIPLE_SELECTION 6
+
+
+#if !defined(PACKAGEID)
+#define PACKAGEID "org.tizen.wallpaper-ui-service"
+#endif
+
+#if !defined(EDJDIR)
+#define EDJDIR "/usr/apps/org.tizen.wallpaper-ui-service/res/edje"
+#endif
+
+#if !defined(ICONDIR)
+#define ICONDIR "/usr/apps/org.tizen.wallpaper-ui-service/res/icons"
+#endif
+
+#define EXTRA_KEY_FILE "file_path"
+#define EXTRA_KEY_POPUP_TYPE "popup_type"
+
+typedef enum {
+	WALLPAPER_POPUP_TYPE_NONE = 0,
+	WALLPAPER_POPUP_TYPE_SELECTION,
+	WALLPAPER_POPUP_TYPE_THEME,
+} wallpaper_popup_type;
+
+
+
+#define EXTRA_KEY_WALLPAPER_TYPE "wallpaper_type"
+
+#define EXTRA_DATA_WP_DEFAULT "default"
+#define EXTRA_DATA_WP_MULTI	"multi"
+
+typedef enum {
+	WALLPAPER_TYPE_DEFAULT = 0,
+	WALLPAPER_TYPE_GALLERY,
+	WALLPAPER_TYPE_MULTIPLE,
+	WALLPAPER_TYPE_MAX
+} wallpaper_img_type;
+
+
+#define EXTRA_FROM_KEY "from"
+
+typedef enum {
+	WALLPAPER_POPUP_FROM_NONE = 0,
+	WALLPAPER_POPUP_FROM_HOME_SCREEN,
+	WALLPAPER_POPUP_FROM_LOCK_SCREEN,
+	WALLPAPER_POPUP_FROM_SETTING,
+	WALLPAPER_POPUP_FROM_GALLERY,
+} wallpaper_popup_from;
+
+
+#define EXTRA_KEY_SETAS "setas-type"
+
+#define EXTRA_DATA_HOMESCREEN "Homescreen"
+#define EXTRA_DATA_LOCKSCREEN	"Lockscreen"
+#define EXTRA_DATA_HOME_N_LOCKSCREEN	"Home&Lockscreen"
+
+typedef enum {
+	WALLPAPER_POPUP_SETAS_NONE = 0,
+	WALLPAPER_POPUP_SETAS_HOMESCREEN,
+	WALLPAPER_POPUP_SETAS_LOCKSCREEN,
+	WALLPAPER_POPUP_SETAS_HOME_N_LOCKSCREEN
+} wallpaper_popup_menu;
+
+
+#define DiSABLE_CROP_VIEW 0
+#define DISABLE_MULTISELECTION 1
+
+typedef struct _popup_colortheme_data {
+	Evas_Object *popup;
+
+	int theme_index;
+	char *file_path;
+} popup_colortheme_data;
+
+typedef struct _popup_wallpaper_data {
+	Evas_Object *win_main;
+	Evas_Object *base;
+
+	Evas_Object *act_pop;
+	int launch_from;
+	int setas_type;
+} popup_wallpaper_data;
+
+typedef struct page_data {
+	Evas_Object *scroller;
+	Evas_Object *index;
+	Evas_Object *page[2];
+	Elm_Object_Item *last_it;
+	Elm_Object_Item *new_it;
+	int current_page;
+	Evas_Object *home_icon;
+	Evas_Object *lockscreen_icon;
+	Evas_Object *main_layout;
+} page_data_s;
+
+typedef struct preview_page_data {
+	Evas_Object *scroller;
+	Evas_Object *page[6];
+	int current_page;
+} preview_page_data_s;
+
+
+typedef struct _wallpaper_ui_service_appdata {
+
+	Evas_Object *navi_bar;
+	Evas *evas;
+	Evas_Object *win;
+	Evas_Object *layout;
+	int request_type;
+
+	app_control_h service; 	// clone service to reply when terminate
+	popup_wallpaper_data sel_popup_data;
+	popup_colortheme_data color_popup_data;
+	int popup_type;
+	int lock_wallpaper_type;
+
+	unsigned int caller_win_id;
+	unsigned int wallpaper_win_id;
+	page_data_s *pd;
+	Evas_Object *main_layout;
+	Evas_Object *thumnail_layout;
+	Evas_Object *box;
+	Evas_Object *popup;
+	Evas_Object *preview_image;
+	int preview_image_type;
+	Elm_Object_Item *main_nf_it;
+    Evas_Object *gengrid;
+
+	E_DBus_Connection *dbus_conn;
+	E_DBus_Signal_Handler *dbus_home_button_handler;
+
+    char* last_preview_img_path;
+    char current_preview_img_path[MAX_LENGTH_LINE];
+	char saved_img_path[6][MAX_LENGTH_LINE];
+} wallpaper_ui_service_appdata;
+
+#endif
+
+
+/**
+* @}
+*/
+/* End of file */
---
+++ packaging/org.tizen.wallpaper-ui-service.spec
@@ -0,0 +1,116 @@
+%define PREFIX    /usr/apps/%{name}
+%define RESDIR    %{PREFIX}/res
+%define PREFIXRW  /opt/usr/apps/%{name}
+
+Name:       org.tizen.wallpaper-ui-service
+Summary:    wallpaper-ui-service window
+Version:    0.0.1
+Release:    1
+Group:      utils
+License:    Apache-2.0
+Source0:    %{name}-%{version}.tar.gz
+
+%if "%{?tizen_profile_name}" == "wearable"
+ExcludeArch: %{arm} %ix86 x86_64
+%endif
+
+%if "%{?tizen_profile_name}"=="tv"
+ExcludeArch: %{arm} %ix86 x86_64
+%endif
+
+BuildRequires: pkgconfig(capi-appfw-application)
+BuildRequires: pkgconfig(capi-system-system-settings)
+BuildRequires: pkgconfig(dlog)
+BuildRequires: pkgconfig(elementary)
+BuildRequires: pkgconfig(vconf)
+BuildRequires: pkgconfig(feedback)
+BuildRequires: pkgconfig(efl-extension)
+BuildRequires: pkgconfig(feedback)
+BuildRequires: pkgconfig(ui-gadget-1)
+BuildRequires: pkgconfig(notification)
+BuildRequires: pkgconfig(capi-content-media-content)
+BuildRequires: pkgconfig(edbus)
+BuildRequires: cmake
+BuildRequires: edje-tools
+BuildRequires: gettext-tools
+BuildRequires: hash-signer
+
+Requires(post): /usr/bin/vconftool
+
+%description
+wallpaper-ui-service window.
+
+%prep
+%setup -q
+
+%build
+%define BRANCH "SDK"
+
+%if 0%{?tizen_build_binary_release_type_eng}
+export CFLAGS="$CFLAGS -DTIZEN_ENGINEER_MODE"
+export CXXFLAGS="$CXXFLAGS -DTIZEN_ENGINEER_MODE"
+export FFLAGS="$FFLAGS -DTIZEN_ENGINEER_MODE"
+export CFLAGS="$CFLAGS -DTIZEN_DEBUG_ENABLE"
+export CXXFLAGS="$CXXFLAGS -DTIZEN_DEBUG_ENABLE"
+export FFLAGS="$FFLAGS -DTIZEN_DEBUG_ENABLE"
+%endif
+
+LDFLAGS+="-Wl,--rpath=%{PREFIX}/lib -Wl,--as-needed";export LDFLAGS
+CFLAGS+=" -fvisibility=hidden -fvisibility-inlines-hidden"; export CFLAGS
+CXXFLAGS+=" -fvisibility=hidden -fvisibility-inlines-hidden"; export CXXFLAGS
+FFLAGS+=" -fvisibility=hidden -fvisibility-inlines-hidden"; export FFLAGS
+
+cmake . -DCMAKE_INSTALL_PREFIX=%{PREFIX} -DBRANCH=%{BRANCH} -DCMAKE_INSTALL_PREFIXRW=%{PREFIXRW} \
+
+make %{?jobs:-j%jobs}
+
+%install
+rm -rf %{buildroot}
+%make_install
+
+if [ ! -d /opt/usr/share/lockscreen/wallpaper_list ]
+then
+	mkdir -p %{buildroot}/opt/usr/share/lockscreen/wallpaper_list
+fi
+
+mkdir -p %{buildroot}/usr/share/license
+cp -f LICENSE %{buildroot}/usr/share/license/%{name}
+mkdir -p %{buildroot}/opt/usr/apps/org.tizen.wallpaper-ui-service/shared
+mkdir -p %{buildroot}/opt/usr/apps/org.tizen.wallpaper-ui-service/shared/res
+mkdir -p %{buildroot}/opt/usr/apps/org.tizen.wallpaper-ui-service/data
+mkdir -p %{buildroot}/opt/usr/apps/org.tizen.wallpaper-ui-service/data/wallpaper
+
+%define tizen_sign 1
+%define tizen_sign_base /usr/apps/org.tizen.wallpaper-ui-service
+%define tizen_sign_level platform
+%define tizen_author_sign 1
+%define tizen_dist_sign 1
+
+%clean
+rm -rf %{buildroot}
+
+%post
+
+#usr/bin/signing-client/hash-signer-client.sh -a -d -p platform /usr/apps/org.tizen.wallpaper-ui-service
+
+%postun -p /sbin/ldconfig
+
+%files
+%manifest org.tizen.wallpaper-ui-service.manifest
+%defattr(-,root,root,-)
+%{PREFIX}/bin/*
+/usr/apps/org.tizen.wallpaper-ui-service/res/locale/*
+%{RESDIR}/icons/*
+%{RESDIR}/edje/*
+/usr/share/packages/org.tizen.wallpaper-ui-service.xml
+/usr/share/license/%{name}
+/etc/smack/accesses.d/%{name}.efl
+/usr/apps/org.tizen.wallpaper-ui-service/author-signature.xml
+/usr/apps/org.tizen.wallpaper-ui-service/signature1.xml
+%attr(-,app,app) %dir /opt/usr/apps/org.tizen.wallpaper-ui-service/shared
+%attr(-,app,app) %dir /opt/usr/apps/org.tizen.wallpaper-ui-service/shared/res
+%attr(-,app,app) %dir /opt/usr/apps/org.tizen.wallpaper-ui-service/data
+%attr(-,app,app) %dir /opt/usr/apps/org.tizen.wallpaper-ui-service/data/wallpaper
+
+%attr(755,app,app)/opt/usr/share/lockscreen
+%attr(755,app,app)/opt/usr/share/lockscreen/wallpaper_list
